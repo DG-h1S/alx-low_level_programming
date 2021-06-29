98dbe1a24e1eb54f96e0903be796fbcecba534d9
DG-h1s <yhwhsdg@gmail.com>
2021-06-29 13:43:05 -0700

    fixed 7-putchar

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,32 @@
 #include "holberton.h"
-
 /**
- * puts_half - Swaps integers wih pointers.
+ * puts_half - prints half of a string
+ * @str: the string to print half
  *
- * @str: is a pointer to a char
  *
- * Return: Always 0.
  */
-
 void puts_half(char *str)
 {
-int i, j;
+  int i = 0;
+  int j;
 
-	while (*(str + i) != '\0')
-	i++;
-
-	if (i % 2 != 0)
-		for (j = ((i + 1) / 2); *(str + j) != '\0'; j++)
-			_putchar(*(str + j));
-	else if (i % 2 == 0)
-		for (j = (i / 2); *(str + j) != '\0'; j++)
-			_putchar(*(str + j));
-_putchar('\n');
+  while (str[i] != '\0')
+    {
+      i++;
+    }
+  if (i % 2 == 0)
+    {
+      i = i / 2;
+    }
+  else
+    {
+      i = (i + 1) / 2;
+    }
+  while (str[i] != '\0')
+    {
+      j = str[i];
+      i++;
+      _putchar(j);
+    }
+  _putchar('\n');
 }
