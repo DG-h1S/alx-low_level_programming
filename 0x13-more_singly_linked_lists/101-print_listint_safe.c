23b1d9f596262ed15ae4c66e2676f493962d7f69
DG-h1s <yhwhsdg@gmail.com>
2021-09-03 01:57:15 -0700

    new lines added

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,14 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print_listint_safe - prints a list
- * @head: address of pointer to first node
- *
- * Return: address of head
- */
+* print_listint_safe - prints a list
+* @head: address of pointer to first node
+*
+* Return: address of head
+*/
+
 size_t print_listint_safe(const listint_t *head)
 {
 	if (head)
